e1bcb83b0c85aa0b106fb095dae33da86c39408c
kayahr <k@ailis.de>
2024-06-04 12:54:50 +0000

    Deploying to gh-pages from @ kayahr/stackblur@b85ecce0683c6a2ec9d7cc866d3db73b25154b3b 🚀

---
+++ assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslLyzE1MT3VJLEn0ycxOVdJRKkgsyVCyUsrMK0ktSktMTi3WR1Ghl1GSm6Oko5SdmZeiZGVkalarAzcqKae0CK4YYVRaaV5ySWZ+XrE+igJUk8xMamMBHuF9A5UAAAA="
